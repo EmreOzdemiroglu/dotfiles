cfce77e9abb14f325de82faf1988cc0241510a12
mreative <ozdemirogluemre@gmail.com>
2025-12-13 01:24:29 +0300

    Initialize Neovim configuration with keymaps and plugins

---
+++ nvim/init_new.lua
@@ -0,0 +1,78 @@
+-- # Keymaps --
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Copy and delete betta --
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Greatest thing --
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/bin/tmux-sessionizer<CR>")
+
+-- # Sets --
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  -- rose-pine colorscheme
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto",
+        dark_variant = "main",
+      })
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+
+  -- comment2code
+  {
+    dir = "/Users/mreative/dev/comment2code.nvim",
+    config = function()
+      require("comment2code").setup({
+        model = "opencode/big-pickle",
+        mode = "auto_linear",
+        debounce_ms = 500,
+        keymaps = {
+          manual_trigger = "<leader>ai",
+          process_all = "<leader>aA",
+        },
+      })
+    end,
+  },
+}, {
+  install = { colorscheme = { "rose-pine" } },
+  checker = { enabled = true },
+})
